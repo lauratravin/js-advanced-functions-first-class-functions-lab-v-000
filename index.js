090d2c02a1417604f2dafb3d7c0391d1a94783af
Laura Messa Travin <laura.travin@gmail.com>
2021-05-03 22:09:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ const createFareMultiplier= function(players){
     }
 }
 //createFareMultiplier(4)(4)  this return 16
+
+const fareDoubler= createFareMultiplier(2) //this is the value for players
+//
+// fareDoubler(3) //3 is value, the 2 is players
+// 6
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function (drivers, returnLastTwoDrivers ){
+   return returnLastTwoDrivers(drivers)
+}
